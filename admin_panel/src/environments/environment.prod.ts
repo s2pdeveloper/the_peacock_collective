761d757f791c9decd2e3cf6a4a85185080767829
RJSingh1997 <rohitjsingh16@gmail.com>
2024-05-10 12:22:50 +0530

    changes

--- admin_panel/src/environments/environment.prod.ts
+++ admin_panel/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import packageInfo from "../../package.json";
 export const environment = {
   appVersion: packageInfo.version,
   websiteUrl: "https://jhumkaplanet.com/catalogoue?productId=",
-  baseUrl: "https://peacockadmin.s2pedutech.com/v1/admin/",
+  baseUrl: "./v1/admin/",
   production: true,
 };
