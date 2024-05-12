cd3091c883394ec1d250baf240849bcd4ad46bb5
RJSingh1997 <rohitjsingh16@gmail.com>
2024-05-12 09:46:33 +0530

    new change

---
+++ backend/website.js
@@ -0,0 +1,91 @@
+require('dotenv').config();
+const express = require('express');
+const compression = require('compression');
+const logger = require('morgan');
+const path = require('path');
+const { errorHandler } = require('./config/middlewares/error.handler');
+const { authenticateJWT } = require('./config/middlewares/auth.middleware');
+const { createSuperAdmin } = require('./config/middlewares/utils');
+
+const lusca = require('lusca');
+const cors = require('cors');
+const lodash = require('lodash');
+const _ = require('lodash');
+let fs = require('fs');
+const indexRouter = require('./controllers/index');
+global._ = lodash;
+const app = express();
+app.use(cors('*'));
+// GZIP compress resources served
+app.use(compression());
+app.use(logger('dev'));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+
+/**
+ * Start Express server.
+ */
+
+app.set('port', 1946);
+
+
+const server = app.listen(app.get('port'), () => {
+  console.log(
+    'App is running at http://localhost:%d in %s mode',
+    app.get('port'),
+    app.get('env')
+  );
+  console.log('Press CTRL-C to stop\n');
+});
+
+app.use(lusca.xframe('SAMEORIGIN'));
+app.use(lusca.xssProtection(true));
+app.disable('x-powered-by');
+
+// Static Hosting
+app.use(
+  '/',
+  express.static(path.join(__dirname, '../website/dist'), { maxAge: 31557600000 })
+);
+app.use('/image', express.static(path.join(__dirname, 'assets')));
+app.use('/apk', express.static(path.join(__dirname, 'app-apk')));
+// development console log
+// app.use((req, res, next) => {
+//   if (process.env.ENVIRONMENT === 'development') {
+//     // console.log('req body', req.body);
+//     // console.log('req query', req.query);
+//     // console.log('authorization', req.headers.authorization);
+//   }
+//   next();
+// });
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+});
+// Routes
+// app.use(expressValidator());
+app.use(authenticateJWT);
+app.use('/', indexRouter);
+app.use(errorHandler);
+
+// setTimeout(() => {
+createSuperAdmin();
+// }, 10000);
+
+app.use('*', (req, res) => {
+  res.status(404).json({
+    success: 'false',
+    message: 'Page not found',
+    error: {
+      statusCode: 404,
+      message: 'You reached a route that is not defined on this server',
+    },
+  });
+});
+let dir = ['./assets/images', './assets/customerImage', './assets/userImage'];
+
+for (let i = dir.length - 1; i >= 0; i--) {
+  let x = dir[i];
+  if (!fs.existsSync(x)) {
+    fs.mkdirSync(x, { recursive: true });
+  }
+}
