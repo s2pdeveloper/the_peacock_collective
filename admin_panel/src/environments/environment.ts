380904611d913a8d9fb0318cd54687eeb6f0ee53
RJSingh1997 <rohitjsingh16@gmail.com>
2024-06-15 11:10:04 +0530

    new chnage

--- admin_panel/src/environments/environment.ts
+++ admin_panel/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   websiteUrl: "https://jhumkaplanet.com/catalogoue?productId=",
-  baseUrl: "https://peacockadmin.s2pedutech.com/api/v1/admin/",
+  baseUrl: "https://admin.peacockcollective.in/api/v1/admin/",
   // baseUrl: "https://billing.jhumkaplanet.com/v1/admin/",
   production: false,
 };
