9654ae1e636017c565c06e90cc148d7654ebb2b6
Aayush849 <ayushamnerkar9@gmail.com>
2025-01-18 13:03:48 +0530

    done

--- admin_panel/src/environments/environment.ts
+++ admin_panel/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  
   websiteUrl: "https://jhumkaplanet.com/catalogoue?productId=",
   // baseUrl: "http://localhost:1945/api/v1/admin/",
   baseUrl: "https://admin.peacockcollective.in/api/v1/admin/",
